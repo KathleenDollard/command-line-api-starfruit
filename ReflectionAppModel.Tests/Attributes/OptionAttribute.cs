19d07e8cd8a507d0cff520cc45d1ba21368b65f8
Kathleen Dollard <kdollard@microsoft.com>
2020-05-23 05:53:44 -0700

    Option arguments are now supported and tested

--- ReflectionAppModel.Tests/Attributes/OptionAttribute.cs
+++ ReflectionAppModel.Tests/Attributes/OptionAttribute.cs
@@ -14,6 +14,9 @@ namespace System.CommandLine.ReflectionAppModel.Tests
         public string Name { get; set; }
         public bool IsHidden { get; set; }
         public bool OptionRequired { get; set; }
+        public string ArgumentName { get; set; }
+        public string ArgumentDescription { get; set; }
         public bool ArgumentRequired { get; set; }
+        public bool ArgumentIsHidden { get; set; }
     }
 }
