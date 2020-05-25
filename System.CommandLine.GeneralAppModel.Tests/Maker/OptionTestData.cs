5c13b1139eabd74171f75f0a0663503e562c56cf
Kathleen Dollard <kathleen.a.dollard@gmail.com>
2020-05-25 13:27:28 -0600

    Updated testing for Maker tests (#44)
    * First new maker tests passing, no unexpected file changes
    
    * WIP
    
    * Tests for primitivesproperties for done and passing
    
    * Arity tests in place
    
    * Add Argument DefaultValue test
    
    * Add SubCommand test
    
    * Skipping test where correct outcome is unclear

---
+++ System.CommandLine.GeneralAppModel.Tests/Maker/OptionTestData.cs
@@ -0,0 +1,55 @@
+﻿using FluentAssertions.Execution;
+using System.Collections.Generic;
+using System.CommandLine.GeneralAppModel.Descriptors;
+using System.CommandLine.Parsing;
+using System.Linq;
+
+namespace System.CommandLine.GeneralAppModel.Tests.Maker
+{
+    public class OptionBasicsTestData : MakerCommandTestDataBase
+    {
+        public OptionBasicsTestData(string name, string description, string[] aliases, bool isHidden, bool required)
+              : base(new CommandDescriptor(null, null) { Name = DummyCommandName })
+        {
+            base.Descriptor.Options.Add(
+                        new OptionDescriptor(null, null)
+                        {
+                            Name = name,
+                            Description = description,
+                            Aliases = aliases,
+                            IsHidden = isHidden,
+                            Required = required
+                        }
+                    );
+            Name = name;
+            Description = description;
+            Aliases = aliases;
+            IsHidden = isHidden;
+            Required = required;
+        }
+
+        public string Name { get; }
+        public string Description { get; }
+        public string[] Aliases { get; }
+        public bool IsHidden { get; }
+        public bool Required { get; }
+
+        public override void Check(Command  command)
+        {
+            var actual = command.Options.FirstOrDefault();
+            var name = Name.ToKebabCase().ToLowerInvariant();
+
+            var expectedAliases = Aliases is null
+                                  ? new string[] { name }
+                                  : Aliases.Prepend(name).ToArray();
+            using var scope = new AssertionScope();
+            actual.Should().NotBeNull()
+                       .And.HaveName(name)
+                       .And.HaveDescription(Description)
+                       .And.HaveAliases(expectedAliases)
+                       .And.HaveIsHidden(IsHidden)
+                       .And.HaveRequired(Required);
+        }
+    }
+
+}
---
+++ System.CommandLine.GeneralAppModel.Tests/NewMakerTests.cs
@@ -0,0 +1,87 @@
+﻿using System.CommandLine.GeneralAppModel.Tests.Maker;
+using System.Linq;
+using Xunit;
+
+namespace System.CommandLine.GeneralAppModel.Tests
+{
+    public class NewMakerTests
+    {
+        public const string name = "Fred";
+        public const string nameForEmpty = "NameIsRequired";
+        public const string desc = "This is awesome!";
+        public const string aliasAsStringMuitple = "x,y,z";
+        public const string aliasAsStringSingle = "a";
+
+        [Theory]
+        [InlineData(name, desc, aliasAsStringMuitple, true, true)]
+        [InlineData(name, desc, aliasAsStringSingle, false, false)]
+        [InlineData(nameForEmpty, null, null, false, false)]
+        public void CommandBasicsTests(string name, string description, string aliasesAsString, bool isHidden, bool treatUnmatchedTokensAsErrors)
+        {
+            var aliases = aliasesAsString is null
+                          ? null
+                          : aliasesAsString.Split(",").Select(s => s.Trim()).ToArray();
+            var data = new CommandBasicsTestData(name, description, aliases, isHidden, treatUnmatchedTokensAsErrors);
+            var command = CommandMaker.MakeCommand(data.Descriptor);
+            data.Check(command);
+        }
+
+        [Theory]
+        [InlineData(name, desc, aliasAsStringMuitple, true, true)]
+        [InlineData(name, desc, aliasAsStringSingle, false, false)]
+        [InlineData(nameForEmpty, null, null, false, false)]
+        public void OptionBasicsTests(string name, string description, string aliasesAsString, bool isHidden, bool required)
+        {
+            var aliases = aliasesAsString is null
+                          ? null
+                          : aliasesAsString.Split(",").Select(s => s.Trim()).ToArray();
+            var data = new OptionBasicsTestData(name, description, aliases, isHidden, required);
+            var command = CommandMaker.MakeCommand(data.Descriptor);
+            data.Check(command);
+        }
+
+        [Theory]
+        [InlineData(name, desc, aliasAsStringMuitple, true, typeof(string))]
+        [InlineData(name, desc, aliasAsStringSingle, false, typeof(string))]
+        [InlineData(nameForEmpty, null, null, false, null)]
+        public void ArgumentBasicsTests(string name, string description, string aliasesAsString, bool isHidden, Type argumentType)
+        {
+            var aliases = aliasesAsString is null
+                          ? null
+                          : aliasesAsString.Split(",").Select(s => s.Trim()).ToArray();
+            var data = new ArgumentBasicsTestData(name, description, aliases, isHidden, argumentType);
+            var command = CommandMaker.MakeCommand(data.Descriptor);
+            data.Check(command);
+        }
+
+        [Theory(Skip ="False case is failing")]
+        [InlineData(true, 0, 5)]
+        [InlineData(false, null, null)]
+        [InlineData(true, 2, 2)]
+        public void ArgumentArityTests(bool isSet, int? minValue, int? maxValue)
+        {
+            var data = new ArgumentArityTestData(isSet, minValue, maxValue);
+            var command = CommandMaker.MakeCommand(data.Descriptor);
+            data.Check(command);
+        }
+
+        [Theory]
+        [InlineData(true, 42)]
+        [InlineData(true, "Fred")]
+        [InlineData(false, null)]
+        public void ArgumentDefaultValueTests(bool isSet, object defaultValue)
+        {
+            var data = new ArgumentDefaultValueTestData(isSet, defaultValue);
+            var command = CommandMaker.MakeCommand(data.Descriptor);
+            data.Check(command);
+        }
+
+        [Fact]
+        public void CommandWithOneSubCommandTests()
+        {
+            var data = new CommandOneSubCommandTestData(name);
+            var command = CommandMaker.MakeCommand(data.Descriptor);
+            data.Check(command);
+        }
+    }
+}
